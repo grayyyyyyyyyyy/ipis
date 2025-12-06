6c3874e1b1bb4dfb78189fd7f3efa0d5b1e557d1
serii <serjvlasov0712@gmail.com>
2025-12-06 15:55:17 +0300

    doc: add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,13 +2,16 @@
 
 using namespace std;
 
+// main - точка входа в программу
 int main() {
-	std::string name;
+	std::string name; // переменная для хранения имени пользователя
 
+	// запрашиваем ввод от пользователя 
 	std::cout << "Enter your name: ";
-	std::cin >> name;
+	std::cin >> name; // читаем ввод пользователя
 
+	// выводим приветственное сообщение
 	std::cout << "Hello world from " << name << std::endl;
 
-	return 0;
+	return 0; // сигнал об успешном выполнении программы
 }
